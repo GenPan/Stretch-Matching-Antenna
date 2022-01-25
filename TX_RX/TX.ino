ae94c776103b037a8533bddb0e782efef0d829db
GenPan <trantino48@gmail.com>
2022-01-25 16:40:18 +0100

    Example code

---
+++ TX_RX/TX.ino
@@ -0,0 +1,37 @@
+/* 1byte 433Mhz TRANSMITTER example.
+/* Tutorial link: http://electronoobs.com/eng_arduino_tut99.php
+ * Code: http://electronoobs.com/eng_arduino_tut99_code1.php
+ * Schematic: http://electronoobs.com/eng_arduino_tut99_sch1.php
+ * Youtube Channel: http://www.youtube/c/electronoobs   
+// Arduino          433Mhz Tx
+// GND              GND
+// 5V               VCC
+// D3               Data
+*/
+
+#include <VirtualWire.h> //Download it here: http://electronoobs.com/eng_arduino_virtualWire.php
+#define size 1
+int pot = A0;
+byte TX_buffer[size]={0};
+byte i;
+
+
+void setup() 
+{
+  // virtual wire
+  vw_set_tx_pin(3); // pin
+  vw_setup(2000); // bps
+  for(i=0;i<size;i++)
+  {
+     TX_buffer[i]=i;
+  }
+}
+
+void loop()
+{ 
+  int val = map(analogRead(pot),0,1024,0,255);
+  TX_buffer[0] = val;  
+  vw_send(TX_buffer, size); 
+  vw_wait_tx(); 
+  delay(500);   
+}
